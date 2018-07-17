dca9dc4dbea4e496c719a68f56bacc73fe12648c
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-07-17 20:08:47 +0200

    add install-disk-on-box-debian.sh

---
+++ vagrant/install-disk-on-box-debian.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# create one partition hole disk
+
+# Exit immediately if a command returns a non-zero status
+set -e
+
+DEVICE="sdc"
+MOUNT_POINT="/cacheC"
+
+function create_partition_table() {
+
+	sudo fdisk -u "/dev/$1" <<EOF
+n
+p
+1
+
+
+w
+EOF
+
+	sync
+
+}
+
+function make_filesystem() {
+
+	sudo mkfs.ext4 "/dev/${1}1"
+
+}
+
+function create_mount_point() {
+
+	sudo mkdir -p "${1}"
+
+}
+
+function mount_device_on_mount_point() {
+
+	sudo mount -t ext4 "/dev/${1}1" "${2}"
+
+}
+
+function create_etc_ftab_entry() {
+
+	UUID="$(grep </dev/disk/by-uuid "${1}"1 | awk '{print $9}')"
+	echo "UUID=${UUID} /${2}               ext4    errors=remount-ro 0       1" | sudo tee -a /etc/fstab
+
+}
+
+# check if running on virtual box
+if (lsmod | grep vboxguest); then
+
+	echo "script run on a virtual box"
+
+	create_partition_table "${DEVICE}"
+	make_filesystem "${DEVICE}"
+	create_mount_point "${MOUNT_POINT}"
+	mount_device_on_mount_point "${DEVICE}" "${MOUNT_POINT}"
+	create_etc_ftab_entry "${DEVICE}" "${MOUNT_POINT}"
+	exit 0
+else
+
+	echo "No vboxguest kernel module found"
+	echo "ERROR This script should run only of a virtual box"
+	exit 1
+
+fi
