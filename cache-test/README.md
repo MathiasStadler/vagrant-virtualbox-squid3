99e98bdf00f34f40583628caca9a960b9cd0b2b3
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-07-17 10:48:44 +0200

    init cache test

---
+++ cache-test/README.md
@@ -0,0 +1,15 @@
+# test cache with a web crawler
+
+## from here
+
+```bash
+https://gist.github.com/antoineMoPa/ada42dcfc96197e38dc8c4df363aed72
+```
+
+## usage
+
+```bash
+time ./crawl.sh <start-url> <proxy> <proxy-ip>
+# e.g.
+time ./crawl.sh http://www.w3.org/TR/SVG11/feature#BasicStructure 192.168.178.131 3128
+```
---
+++ cache-test/crawl.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# bash web crawler
+# $ bash crawl.sh http://example.com
+
+# from here
+# https://gist.github.com/antoineMoPa/ada42dcfc96197e38dc8c4df363aed72
+
+WORK_DIR="work"
+
+rm -fr "$WORK_DIR"
+
+mkdir -p "$WORK_DIR"
+
+URL_TEXT="${WORK_DIR}/urls.txt"
+SUB_URLS_TXT="${WORK_DIR}/sub-urls.txt"
+SUB_2_URLS_TXT="${WORK_DIR}/sub-2-urls.txt"
+
+site=$1
+proxy_ip=$2
+proxy_port=$3
+
+function visit() {
+	echo visiting "$1"
+
+	# get URLs
+	curl -x "$3:$4" -silent "$1" | grep href=\" | grep "http://" | grep -o "http:\\/\\/[^\"]*" | sed 's/<.*//' >>"$2"
+}
+
+visit "$site" "$URL_TEXT" "$proxy_ip" "$proxy_port"
+
+while
+	read -r line
+do
+	(echo "$line" | grep 'zip') && continue
+	(echo "$line" | grep 'pdf') && continue
+	visit "$line" "$SUB_URLS_TXT" "$proxy_ip" "$proxy_port"
+done <"$URL_TEXT"
+
+while
+	read -r line
+do
+	(echo "$line" | grep 'zip') && continue
+	(echo "$line" | grep 'pdf') && continue
+	visit "$line" "$SUB_2_URLS_TXT" "$proxy_ip" "$proxy_port"
+done <$SUB_URLS_TXT
