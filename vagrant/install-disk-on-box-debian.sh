2bde6af3b4db17d9e0a7abf1d753a266cc8d37ca
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-07-17 20:54:22 +0200

    add counter to wait

--- vagrant/install-disk-on-box-debian.sh
+++ vagrant/install-disk-on-box-debian.sh
@@ -3,5 +3,19 @@
 # Exit immediately if a command returns a non-zero status
 set -e
 
+# wait on parallel provision
+# wait max 10
+counter=1
+until
+	[ ! -e /home/vagrant/settings/setting-disk-on-box-debian.sh ]
+do
+	if [ $counter -le 10 ]; then
+		break
+	fi
+	echo "Wait for next seconds"
+	sleep 1
+	((counter++))
+done
+
 sudo /home/vagrant/settings/setting-disk-on-box-debian.sh "sdb" "cacheB"
 sudo /home/vagrant/settings/setting-disk-on-box-debian.sh "sdc" "cacheC"
