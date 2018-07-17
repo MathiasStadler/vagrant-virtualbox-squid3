bd16238363f6524b78a7ef4d2f3dd97b243d8b7d
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-07-17 20:42:38 +0200

    add sudo

--- vagrant/install-disk-on-box-debian.sh
+++ vagrant/install-disk-on-box-debian.sh
@@ -3,5 +3,5 @@
 # Exit immediately if a command returns a non-zero status
 set -e
 
-/home/vagrant/settings/setting-disk-on-box-debian.sh "sdb" "cacheB"
-/home/vagrant/settings/setting-disk-on-box-debian.sh "sdc" "cacheC"
+sudo /home/vagrant/settings/setting-disk-on-box-debian.sh "sdb" "cacheB"
+sudo /home/vagrant/settings/setting-disk-on-box-debian.sh "sdc" "cacheC"
