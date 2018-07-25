bfcab9dbe395cd89c9d3dce5f3473edff125a4bb
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-07-25 13:39:00 +0200

    save

--- vagrant/install-test-ccache-debian.sh
+++ vagrant/install-test-ccache-debian.sh
@@ -6,6 +6,13 @@
 readonly TEST_FILE="hello_world"
 readonly TEST_FILE_CPP="$TEST_FILE.cpp"
 readonly TEST_FILE_O="$TEST_FILE.o"
+
+function install_necessary_packages() {
+
+	sudo apt-get update && sudo apt-get install -y --no-install-recommends build-essential
+
+}
+
 function create_test_file() {
 
 	cat <<EOF >"${TEST_FILE_CPP}"
@@ -22,7 +29,9 @@ EOF
 
 function compile_test_file() {
 
-	ccache g++ -g -pipe -o $TEST_FILE_O -c $TEST_FILE_CPP
+	export CC="/usr/bin/gcc"
+	export CXX="/usr/bin/g++"
+	ccache gcc -g -pipe -o $TEST_FILE_O -c $TEST_FILE_CPP
 
 }
 
@@ -31,6 +40,7 @@ function call_ccache_statistic() {
 	ccache -s
 }
 
+install_necessary_packages
 create_test_file
 compile_test_file
 call_ccache_statistic
