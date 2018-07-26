cba00d881a2b548457dae0a6dedcd480762d7665
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-07-26 14:05:59 +0200

    clean up

--- use-case-recommended-configuration/use-case-squid3-debian-recommended.sh
+++ use-case-recommended-configuration/use-case-squid3-debian-recommended.sh
@@ -3,20 +3,6 @@
 # Exit immediately if a command returns a non-zero status
 set -e
 
-#readonly TEMP_DIR="/tmp"
-
-# BUILD_DIR for tar extract, make ...
-#readonly BUILD_DIR=$TEMP_DIR
-
-#readonly LOG_FILE="${BUILD_DIR}/build_$$_$(date +%F_%H-%M-%S).log"
-
-# CONSTANTS
-
-#readonly INSTALL_PACKAGE_FINAL_LIST="${BUILD_DIR}/install-final-package.list"
-#readonly INSTALL_DEFAULT_PACKAGE="${BUILD_DIR}/install-default-package.list"
-#readonly INSTALL_PACKAGE_USE_CASE="${BUILD_DIR}/install-package-use-case.list"
-#readonly SQUID_CONF="${BUILD_DIR}/squid.conf"
-
 # import project variables
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; else
@@ -51,32 +37,14 @@ else
 fi
 
 # shellcheck disable=SC1090,SC1091
-# source "$SETTINGS_DIR/ccache-handling-debian.sh"
-
-# shellcheck disable=SC1090,SC1091
-# source "$SETTINGS_DIR/squid_version.sh"
-
-# shellcheck disable=SC1090,SC1091
-# source "$SETTINGS_DIR/squid-handling-debian.sh"
-
-# shellcheck disable=SC1090,SC1091
-# source "$SETTINGS_DIR/compare_package_list.sh"
-
-# shellcheck disable=SC1090,SC1091
 source "$SETTINGS_DIR/use-case-squid3-debian-master.sh"
-
-# squid configuration
-# https://wiki.squid-cache.org/SquidFaq/ConfiguringSquid
-
-# minimal 3.5 config
-# https://wiki.squid-cache.org/SquidFaq/ConfiguringSquid#Squid-3.5_default_config
 
 #######################################
 # Modify for use case start############
 #######################################
 function squid-use-case-package-list() {
 
-	cat <<EOF >${INSTALL_PACKAGE_USE_CASE}
+	cat <<EOF >"${INSTALL_PACKAGE_USE_CASE}"
 	# empty
 	# add only packages for the use case
 	# entry package name without default bash comment sign
@@ -88,6 +56,7 @@ EOF
 function squid-use-case-additional-autoconf-configure() {
 
 	# add only autoconf config for this use case
+	# shellcheck disable=SC2034
 	array_add_one_configure_options=()
 
 }
@@ -110,7 +79,7 @@ function squid-use-case-check() {
 # Modify for use case end  ############
 #######################################
 
-# call funtion action in use-case-squid3-debian-master.sh
+# call function action in use-case-squid3-debian-master.sh
 action
 
 echo "# Finished"
