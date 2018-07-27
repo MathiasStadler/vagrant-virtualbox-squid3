5ae07eaffe0d2e9f6a71e79d2e9b6a6f42fbe557
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-07-27 16:44:06 +0200

    add settings_dir

--- vagrant/install-bind-debian.sh
+++ vagrant/install-bind-debian.sh
@@ -5,9 +5,39 @@
 # Exit immediately if a command returns a non-zero status
 set -e
 
-# load utility-method.sh
+# import project variables
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; else
+	echo "# OK WORK DIR => $PWD"
+fi
+
+# SETTINGS_DIR="${DIR}/settings"
+SETTINGS_DIR="../../settings"
+if [[ ! -d "$SETTINGS_DIR" ]]; then
+	echo "# ERROR SETTINGS_DIRECTORY NOT found => $SETTINGS_DIR"
+	# SETTINGS_DIR="${DIR}/settings"
+	SETTINGS_DIR="$HOME/settings"
+	if [[ ! -d "$SETTINGS_DIR" ]]; then
+		echo "# ERROR SETTINGS_DIRECTORY NOT found => $SETTINGS_DIR"
+		echo "# EXIT 1"
+		SETTINGS_DIR="/home/vagrant/settings"
+		if [[ ! -d "$SETTINGS_DIR" ]]; then
+			echo "# ERROR SETTINGS_DIRECTORY NOT found => $SETTINGS_DIR"
+			echo "# EXIT 1"
+			exit 1
+		else
+			echo "# OK SETTINGS_DIRECTORY => $SETTINGS_DIR"
+		fi
+	else
+		echo "# OK SETTINGS_DIRECTORY => $SETTINGS_DIR"
+	fi
+else
+	echo "# OK settings dir $SETTINGS_DIR"
+fi
+
+# load utility-method.sh from same directory
 # shellcheck disable=SC1090,SC1091
-source "./utility-methods.sh"
+source "$SETTINGS_DIR/utility-methods-debian.sh"
 
 # TEMP_DIR
 readonly TEMP_DIR="/tmp"
