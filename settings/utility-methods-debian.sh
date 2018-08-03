67a492fbb56c2de92dff5891ffe7b075bf50a53d
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-08-03 17:07:39 +0200

    test ; test ${PIPESTATUS[0]} -eq 0

--- settings/utility-methods-debian.sh
+++ settings/utility-methods-debian.sh
@@ -179,7 +179,10 @@ function configure-package-new-approach() {
 	cd "${TARGET_DIR}"
 
 	# run configure
-	if ("./$NAME_OF_CONFIG_SCRIPT" "${ARRAY_OF_AUTOCONF_OPTION[@]}" | tee -a "${LOG_FILE}" >/dev/null); then
+	if (
+		"./$NAME_OF_CONFIG_SCRIPT" "${ARRAY_OF_AUTOCONF_OPTION[@]}" | tee -a "${LOG_FILE}" >/dev/null
+		test ${PIPESTATUS[0]} -eq 0
+	); then
 
 		echo "# DEBUG rr $?"
 		echo "# OK $TARGET_DIR/$NAME_OF_CONFIG_SCRIPT ${ARRAY_OF_AUTOCONF_OPTION[*]} run without error" | tee -a "${LOG_FILE}"
