2b745e37c8d47d12322e2009462d45a373eb52e1
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-08-09 13:17:58 +0200

    fix LOG-FILE

--- settings/bind-parameter.sh
+++ settings/bind-parameter.sh
@@ -5,9 +5,12 @@ echo "# OK ${0##*/} loaded" | tee -a "${LOG_FILE}"
 #echo "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}"
 printf "# INFO script %s post load script %s\\n" "$0" "${BASH_SOURCE[@]}"
 
+LOG_FILE="$0_$$_$(date +%F_%H-%M-%S).log"
+
 BIND_BINARY_DEFAULT_PATH="/usr/sbin"
 
 ETC_BIND_NAMED_CONF="/etc/bind/named.conf"
 
+echo "# INFO used parameter LOG_FILE => $LOG_FILE"
 echo "# INFO used parameter BIND_BINARY_DEFAULT_PATH => $BIND_BINARY_DEFAULT_PATH"
 echo "# INFO used parameter ETC_BIND_NAMED_CONF => $ETC_BIND_NAMED_CONF"
