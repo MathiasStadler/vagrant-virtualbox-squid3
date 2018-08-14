6bcc0ac718d56029609a6c75e789f4cbba9bb38c
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-08-14 14:54:13 +0200

    add new test

--- bind-static-zone/static-zone-test.sh
+++ bind-static-zone/static-zone-test.sh
@@ -1,140 +1,18 @@
 #!/bin/bash
 
-# Exit immediately if a command returns a non-zero status
-set -e
+echo "# ACTION run test"
 
-# shellcheck disable=SC1091
-source ../settings/utility-bash.sh
+# generate a 12 char random string
+RANDOM_STRING =$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 
-# call function
-ensure-sudo
+# set variable
+DDNS_NAME_SERVER="127.0.0.1"
+DDNS_ZONE="TEST-$RANDOM_STRING"
 
-SETTINGS="../settings"
+echo "# ACTION create zone"
 
-# shellcheck disable=SC1091
-source ./static-zone-parameter.sh
+bash -x ./static-zone-create.sh "$DDNS_NAME_SERVER" "$DDNS_ZONE"
 
-# shellcheck disable=SC1091
-source "$SETTINGS/utility-dns-debian.sh"
+echo "# ACTION delete zone"
 
-# shellcheck disable=SC1091
-source "$SETTINGS/utility-dns-debian-test.sh"
-
-function test-nsupdate-round-trip-add-record() {
-
-	TEST_FOLDER="/nsupdate_tests"
-
-	echo "#ACTION create sub folder $TEST_FOLDER"
-	mkdir -p "$HOME$TEST_FOLDER"
-
-	# create NSUPDATE_ADD_HOST_SCRIPT
-	NSUPDATE_ADD_HOST_SCRIPT="$HOME$TEST_FOLDER/nsupdate_add_host.sh"
-
-	echo "# ACTION write $NSUPDATE_ADD_HOST_SCRIPT to $HOME$TEST_FOLDER"
-
-	$SUDO tee -a "$NSUPDATE_ADD_HOST_SCRIPT" <<EOF
-#!/bin/bash
-#Defining Variables
-DNS_SERVER="$DDNS_TEST_NAME_SERVER"
-DNS_ZONE="$DDNS_TEST_ZONE."
-HOST="$DDNS_TEST_HOST"
-IP="$DDNS_TEST_IP"
-TTL="60"
-RECORD=" \$HOST \$TTL A \$IP"
-echo "
-server \$DNS_SERVER
-zone \$DNS_ZONE
-debug
-update add \$RECORD
-show
-send" | nsupdate -k $ETC_BIND_DDNS_NSUPDATE_FILE
-EOF
-
-	echo "# ACTION set execute for $NSUPDATE_ADD_HOST_SCRIPT"
-	# execute script NSUPDATE_ADD_HOST_SCRIPT
-	$SUDO chmod +x "$NSUPDATE_ADD_HOST_SCRIPT"
-
-	echo "# ACTION reload zone $DDNS_TEST_ZONE"
-	# activate changes
-
-	echo "# ACTION execute nsupdate of zone $DDNS_TEST_ZONE"
-	if ("$SUDO" "$NSUPDATE_ADD_HOST_SCRIPT"); then
-		echo "# OK nsupdate of zone "
-	else
-		echo "# ERROR nsupdate of zone"
-		echo "# EXIT 1"
-		exit 1
-	fi
-
-}
-
-# call function
-test-nsupdate-round-trip-add-record
-
-# test entry
-EXPECTED_RESULT_OK=0
-COMMAND="get-ip-of-url $DDNS_TEST_HOST $DDNS_TEST_NAME_SERVER"
-EXPECTED_RESULT=$EXPECTED_RESULT_OK
-# call function
-test_function "$COMMAND" "$EXPECTED_RESULT"
-echo "# INFO IP_SERVER => $IP_SERVER"
-
-function test-nsupdate-round-trip-delete-record() {
-
-	# PTR
-	# https://superuser.com/questions/977132/when-using-nsupdate-to-update-both-a-and-ptr-records-why-do-i-get-update-faile
-
-	echo "# INFO call test-nsupdate-round-trip"
-
-	TEST_FOLDER="/nsupdate_tests"
-
-	echo "#ACTION create sub folder $TEST_FOLDER"
-	mkdir -p "$HOME/$TEST_FOLDER"
-
-	# delete test record
-	NSUPDATE_DELETE_RECORD_SCRIPT="$HOME$TEST_FOLDER/nsupdate-delete-record.sh"
-
-	cat <<EOF >"$NSUPDATE_DELETE_RECORD_SCRIPT"
-#!/bin/bash
-#Defining Variables
-DNS_SERVER="localhost"
-DNS_ZONE="$DDNS_TEST_ZONE."
-HOST="test.$DDNS_TEST_ZONE."
-IP="192.168.178.100"
-echo "
-server \$DNS_SERVER
-zone \$DNS_ZONE
-debug
-update delete \$HOST A
-show
-send" | nsupdate -k $ETC_BIND_DDNS_NSUPDATE_FILE
-EOF
-
-	# set execute
-	echo "#ACTION set execute $NSUPDATE_DELETE_RECORD_SCRIPT"
-	chmod +x "$NSUPDATE_DELETE_RECORD_SCRIPT"
-
-	# execute $NSUPDATE_DELETE_RECORD_SCRIPT
-
-	if ($NSUPDATE_DELETE_RECORD_SCRIPT); then
-		echo "# OK nsupdate delete host "
-	else
-		echo "# ERROR nsupdate delete host raise a error"
-		echo "# INFO the return code was $?"
-		echo "# INFO return code => 1:	nsupdate calling error"
-		echo "# INFO return code => 2:	DDNS protocol error"
-		#echo "# EXIT 1"
-		#exit 1
-	fi
-
-}
-
-# call function
-test-nsupdate-round-trip-delete-record
-
-# test entry
-EXPECTED_RESULT_NO_IP_ADDRESS=9
-COMMAND="get-ip-of-url $DDNS_TEST_HOST $DDNS_TEST_NAME_SERVER"
-EXPECTED_RESULT=$EXPECTED_RESULT_OK
-# call function
-test_function "$COMMAND" "$EXPECTED_RESULT_NO_IP_ADDRESS"
+bash -x ./static-zone-delete.sh "$DDNS_NAME_SERVER" "$DDNS_ZONE"
