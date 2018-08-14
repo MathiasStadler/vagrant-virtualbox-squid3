b079826b56074cc7e45f86f8ad83bfcb17fbe40c
Mathias Stadler <mathias@Mathiass-MBP.fritz.box>
2018-08-14 14:55:02 +0200

    fix

--- bind-static-zone/static-zone-test.sh
+++ bind-static-zone/static-zone-test.sh
@@ -3,7 +3,7 @@
 echo "# ACTION run test"
 
 # generate a 12 char random string
-RANDOM_STRING =$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+RANDOM_STRING=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 
 # set variable
 DDNS_NAME_SERVER="127.0.0.1"
